d2ddf89aae019d835eca9df9a415d6f519b99114
roshinie <roshinie.18@cse.mrt.ac.lk>
2020-08-03 21:25:17 +0530

    calculate rectangle detail

--- index.js
+++ index.js
@@ -1,7 +1,4 @@
-var react = {
-    perimeter: (x,y) => (2*(x+y)),
-    area: (x,y) => (x*y)
-};
+var react = require("./rectangle");
 
 function solveRect(l,b){
     console.log("Solving for REctangle with l ="+ l + " "+ "b= "+b);
