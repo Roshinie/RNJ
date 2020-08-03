372e989d6930787b8886e181a694dd227590a90c
roshinie <roshinie.18@cse.mrt.ac.lk>
2020-08-04 00:00:15 +0530

    node callbacks and error handling

--- index.js
+++ index.js
@@ -3,14 +3,17 @@ var react = require("./rectangle");
 function solveRect(l,b){
     console.log("Solving for REctangle with l ="+ l + " "+ "b= "+b);
 
-    if(l <=0 || b<=0){
-        console.log("Dimensions must be greater than zero");
-    }else{
-        console.log("The area of the rectangle is " + react.area(l ,b));
-        console.log("The perimeter of the rectangle is " + react.perimeter(l ,b));
-    }
+    react(l,b,(err,rectangle) =>{
+        if (err){
+            console.log("ERROR : ", err.message);
+        } else{
+            console.log("The area of the rectangle of dimensions l = "+ l+"and b = " +b + " is "+ rectangle.area());
+            console.log("The perimeter of the rectangle of dimensions l = "+ l+"and b = " +b + " is "+ rectangle.Perimeter());
+        }
+    });
+    console.log("This statement is after the call to react");
+
 }
 
 solveRect(10,9);
 solveRect(-10,9);
-solveRect(0,9);
--- rectangle.js
+++ rectangle.js
@@ -1,3 +1,17 @@
-exports.perimeter =(x,y) => (2*(x+y));
+module.exports = (x,y,callback) =>{
+    
+    if(x <=0 || y<=0){
+        setTimeout(() =>
+        callback(new Error("Dimensions must be greater than zero") , null) , 
+        2000);
+    }else{
+        setTimeout(() =>
+        callback(null, 
+            {
+                Perimeter : () => (2*(x+y)),
+                area : () => (x*y)
+            }) , 
+        2000);
 
-exports.area = (x,y) => (x*y);
+    }
+}
