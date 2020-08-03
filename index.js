be92bc28c9c53c72559767e9859a25e86dc462af
roshinie <roshinie.18@cse.mrt.ac.lk>
2020-08-03 14:12:37 +0530

    changes in files

--- index.js
+++ index.js
@@ -1,2 +1,19 @@
-const url = require('url');
-const http = require('http');
+var react = {
+    perimeter: (x,y) => (2*(x+y)),
+    area: (x,y) => (x*y)
+};
+
+function solveRect(l,b){
+    console.log("Solving for REctangle with l ="+ l + " "+ "b= "+b);
+
+    if(l <=0 || b<=0){
+        console.log("Dimensions must be greater than zero");
+    }else{
+        console.log("The area of the rectangle is " + react.area(l ,b));
+        console.log("The perimeter of the rectangle is " + react.perimeter(l ,b));
+    }
+}
+
+solveRect(10,9);
+solveRect(-10,9);
+solveRect(0,9);
